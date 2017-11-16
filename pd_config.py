a4102ea1579faa45cd9997440d00b6eaa3b2cb5a
JaroBelkov <33634857+JaroBelkov@users.noreply.github.com>
2017-11-16 01:09:10 +0000

    Delete pd_config.py

--- pd_config.py
+++
@@ -1,8 +0,0 @@
-# Please, read README.md carefully before editing!
-auto_move = -1
-auto_replay = 1
-ar_delay = 0.7
-is_gui = -1
-diff_level = [0, 2, 2]
-default_who_am_I = 1
-default_main_position = [[0, 12, 0, 0, 12], [3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, -1, -1, -1, -1, 3, -1, -1, -1, -1, -1, -1, -1, -1], [3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, -1, -1, -1, -1, 3, -1, -1, -1, -1, -1, -1, -1, -1]]
